814822910ea5b0a3fe72796b9a04f90c59761e38
Aniket Sharma <aniketsharma312@gmail.com>
2022-05-23 02:01:30 -0600

    commented code, fixed slide feature, added model description feature

--- app.js
+++ app.js
@@ -3,12 +3,15 @@ let translateDiv = document.querySelector('.translate-div')
 let nextButton = document.querySelector('.button-next a')
 let prevButton = document.querySelector('.button-prev a')
 let slides = document.querySelectorAll('.section')
-
+let variableNumber = document.querySelector('.number-variable')
+let buttons = document.querySelectorAll('button')
 // Setting up main variable
+
+
+// Setting up numbers for slide
 let idx = 0;
 
-
-console.log(slides, prevButton, nextButton)
+variableNumber.innerHTML = `01`;
 
 function changeImage() {
     if(idx > slides.length - 1) {
@@ -18,22 +21,37 @@ function changeImage() {
     }
 
     translateDiv.style.transform = `translateY(${-idx * windowHeight}px)`
-}
+};
+
+// Setting up Button Listerners
+nextButton.addEventListener('click', moveForward);
+prevButton.addEventListener('click', movePrevious);
 
 
-nextButton.addEventListener('click', moveForward)
-prevButton.addEventListener('click', movePrevious)
-
-
+// Button function
 function moveForward() {
     idx++
     changeImage()
-    console.log('click')
-}
+    variableNumber.innerHTML = `0${idx + 1}`
+};
 
 function movePrevious() {
     idx--
     changeImage()
-    console.log('click')
+    variableNumber.innerHTML = `0${idx + 1}`
+};
 
-}
+// Use Buttons to open model description
+buttons.forEach((button) => {
+let infoContainer = button.parentElement.previousElementSibling.getElementsByClassName('model-description')[0];
+
+
+button.addEventListener('click', () => {
+ console.log(infoContainer)
+ if (infoContainer.style.left === "-100%") {
+        infoContainer.style.left = "0";
+      } else {
+        infoContainer.style.left = "-100%";
+      }
+})
+});
